2c5cc9fe05ee89d31f30d5351682440863e671da
François Mockers <mockersf@gmail.com>
2020-10-08 23:04:06 +0200

    :arrow_up: updating dependencies

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -5,7 +5,7 @@ pub mod button;
 pub struct ColorScheme;
 
 impl ColorScheme {
-    pub const TEXT: Color = Color::rgb(0.85, 1.0, 0.85);
-    pub const TEXT_DARK: Color = Color::rgb(0.25, 0.35, 0.25);
-    pub const TEXT_DIM: Color = Color::rgb(0.6, 0.6, 0.6);
+    pub const TEXT: Color = Color::rgb_linear(0.85, 1.0, 0.85);
+    pub const TEXT_DARK: Color = Color::rgb_linear(0.25, 0.35, 0.25);
+    pub const TEXT_DIM: Color = Color::rgb_linear(0.6, 0.6, 0.6);
 }
